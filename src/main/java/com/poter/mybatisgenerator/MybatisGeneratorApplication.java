caea7907715497d64027018f7cbc7a6ec66137b4
botao <1011212772@qq.com>
2019-10-21 23:28:18 +0800

    config mybatis mapperScan

--- src/main/java/com/poter/mybatisgenerator/MybatisGeneratorApplication.java
+++ src/main/java/com/poter/mybatisgenerator/MybatisGeneratorApplication.java
@@ -1,8 +1,9 @@
 package com.poter.mybatisgenerator;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@MapperScan("com.poter.mybatisgenerator.mapper")
 @SpringBootApplication
 public class MybatisGeneratorApplication {
 
